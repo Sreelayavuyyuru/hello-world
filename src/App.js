35ee27d3b8395f3da3a238057bdee069e77b05fa
Sreelaya Vuyyuru <sreelayavuyyuru@Sreelayas-MacBook-Air.local>
2021-05-02 15:26:27 +0530

    Updated readme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const name = "Laya";
+  const isLoggedIn = false;
+  // const elements = [] 
+  // const items = []
+  // for (const [index, value] of elements.entries()){" "}
+  //       {items.push(<Element key={index} />)}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> Hello World! </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,3 +27,5 @@ function App() {
 }
 
 export default App;
+
+// true && some expresion will return the expression.
